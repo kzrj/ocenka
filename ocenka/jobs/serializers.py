456ee47083c3dd3cafb87925376582b7fb61b276
kaizerj <kzrster@gmail.com>
2020-07-29 15:51:36 +0800

    job serializer

--- ocenka/jobs/serializers.py
+++ ocenka/jobs/serializers.py
@@ -5,9 +5,9 @@ from jobs.models import Job
 
 
 class JobSerializer(serializers.ModelSerializer):
-	category = serializers.StringRelatedField()
-	zakazchik = serializers.ReadOnlyField(source='zakazchik.profile.nickname')
-	
+    category = serializers.StringRelatedField()
+    zakazchik = serializers.ReadOnlyField(source='zakazchik.profile.nickname')
+    
     class Meta:
         model = Job
         fields = '__all__'
