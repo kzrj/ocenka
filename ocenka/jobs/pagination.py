e5658b1712e7ac063d26e06ccaee10506551b4ad
kaizerj <kzrster@gmail.com>
2020-08-12 17:25:47 +0800

    add cateories to jobs views

---
+++ ocenka/jobs/pagination.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from rest_framework.pagination import PageNumberPagination
+
+
+class JobPagination(PageNumberPagination):
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data['jobs'],
+            'categories'data['categories']: 
+        })
