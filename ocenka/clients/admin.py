1280f345826a1062b1595597283a6ee12fb517d6
kaizerj <kzrster@gmail.com>
2020-08-13 17:56:22 +0800

    test viber sending

--- ocenka/clients/admin.py
+++ ocenka/clients/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from clients.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Profile._meta.fields]
