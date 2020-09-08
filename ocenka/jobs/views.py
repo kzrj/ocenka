9bed6ebf06005bf27098bbb24eca04210a96efa1
kaizerj <kzrster@gmail.com>
2020-09-08 18:03:05 +0800

    viber

--- ocenka/jobs/views.py
+++ ocenka/jobs/views.py
@@ -258,9 +258,10 @@ def viber_view(request):
         text_message = TextMessage(text="Конверсэйшн! Приветствие! Логин!")
         viber.send_messages(viber_request.user.id, [
             text_message, 
-            KeyboardMessage(tracking_data='TRACKING_CREATE_AD_PHONE', 
-                            keyboard=login_keyboard(123),
-                            min_api_version=6)
+            KeyboardMessage(
+                # tracking_data='TRACKING_CREATE_AD_PHONE', 
+                keyboard=login_keyboard(123),
+                min_api_version=6)
             # KeyboardMessage(tracking_data='TRACKING_CREATE_AD_PHONE', 
             #                 keyboard=login_keyboard(viber_request.user.id),
             #                 min_api_version=6)
