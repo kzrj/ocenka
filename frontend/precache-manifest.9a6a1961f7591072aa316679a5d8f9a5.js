273b31e9059694b5d540ece9758c9a737efd13a3
kaizerj <kzrster@gmail.com>
2020-09-10 16:40:15 +0800

    front

---
+++ frontend/precache-manifest.9a6a1961f7591072aa316679a5d8f9a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6332442c5731794b9791429903877f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3abe3ce5827c61694eb4",
+    "url": "/static/js/2.0ab458a3.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.0ab458a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fcb1b333570e6bf1188",
+    "url": "/static/js/main.326d4af7.chunk.js"
+  },
+  {
+    "revision": "d45a2e0481b98f52f2e6",
+    "url": "/static/js/runtime-main.15278f9a.js"
+  }
+]);
